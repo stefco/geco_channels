ac55c838c6af299ec4581fb06204564ea9fb9ea3
Stefan Countryman <stefan.countryman@gmail.com>
2016-06-26 21:06:29 -0400

    add Slave class

--- geco_channels.py
+++ geco_channels.py
@@ -4,6 +4,10 @@
 
 import argparse
 
+def device_type(end_of_channel_name):
+    # TODO: define
+    pass
+
 def is_timing(channel_name):
     """Does this channel name follow the timing system naming conventions?"""
     ch_name_array = channel_name.replace(':','_').split('_')
@@ -17,7 +21,7 @@ class MFO(object):
     def __init__(self, channel_name):
         # only SYS-TIMING objects are connected to MFOs
         if not is_timing(channel_name):
-            raise ValueError("Channel not connected to MFO: " 
+            raise ValueError("Channel not connected to MFO: "
                              + str(channel_name))
         ch_name_array   = channel_name.upper().replace(':','_').split('_',7)
         self.ifo        = ch_name_array[0]
@@ -34,6 +38,38 @@ class MFO(object):
         """
         return self.ifo + ':' + self.subsys + '_' + self.location +  '_'
             + self.mfo + '_' + self.device_id
+
+    def __str__(self):
+        """Get the partial channel name for this MFO. The __repr__ and __str__
+        methods are the same, since string representations of these objects
+        are unambiguous.
+        """
+        return __repr__(self)
+
+class Slave(object):
+    """A Timing Slave Device."""
+    def __init__(self, channel_name):
+        # only SYS-TIMING objects can be members.
+        if not is_timing(channel_name):
+            raise ValueError("Channel not connected to MFO: " 
+                             + str(channel_name))
+        ch_name_array   = channel_name.upper().replace(':','_').split('_',7)
+        self.ifo        = ch_name_array[0]
+        self.subsys     = ch_name_array[1]
+        self.location   = ch_name_array[2]
+        self.mfo        = ch_name_array[3]
+        self.device_id  = ch_name_array[4]
+        self.port       = ch_name_array[6]  # [5] is just word "PORT"
+        self.type       = device_type(ch_name_array[7])
+
+    def __repr__(self):
+        """Get the partial channel name for this MFO. The __repr__ and __str__
+        methods are the same, since string representations of these objects
+        are unambiguous.
+        """
+        return self.ifo + ':' + self.subsys + '_' + self.location +  '_'
+            + self.mfo + '_' + self.device_id + '_PORT_' + self.port + '_'
+            + self.type
 
     def __str__(self):
         """Get the partial channel name for this MFO. The __repr__ and __str__
@@ -78,3 +114,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
